25a289c8342fe2e6ff0d51644d412f6367c8a2e4
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2020-11-24 14:24:06 +0100

    Implement MVC Add view controllers

---
+++ Kanarek/Controllers/ReportControllerThree.swift
@@ -0,0 +1,29 @@
+//
+//  ReportControllerThree.swift
+//  Kanarek
+//
+//  Created by Chris Yarosh on 24/11/2020.
+//
+
+import UIKit
+
+class ReportControllerThree: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
