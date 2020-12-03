13ea7e062b66dfe3b0c609a769e35a5a3c57cf5a
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2020-12-03 19:21:41 +0100

    Implement CLLocationManagerDelegate Implement code for getting constant location updates Add privacy key-value pairs in info.plist Add Background mode - location updates

--- Kanarek/Controllers/MainController.swift
+++ Kanarek/Controllers/MainController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import CoreLocation 
 
 class MainController: UIViewController {
+    
+    let locationManager = CLLocationManager()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -21,9 +24,34 @@ class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.allowsBackgroundLocationUpdates = true // This line is respinsible for background location updates
+    
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func reportButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "GoToReportOne", sender: self)
+    }
+    
 }
 
+//MARK: - LocationManagerDelegate
+extension MainController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.last{
+            
+            print("Automatic:\(location.coordinate.latitude),\(location.coordinate.longitude)")
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
