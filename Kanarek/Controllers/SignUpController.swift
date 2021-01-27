45a21197ee281366e95a2a44acdc5275c4845d44
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2021-01-27 21:50:31 +0100

    Redesign user interface for SignUp view Implement the whole functionality of the signing process Add textField delegates Implement functionality of the checkbox Remake placeholders

--- Kanarek/Controllers/SignUpController.swift
+++ Kanarek/Controllers/SignUpController.swift
@@ -6,27 +6,78 @@
 //
 
 import UIKit
+import Firebase
 
-class SignUpController: UIViewController {
+class SignUpController: UIViewController{
     
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var checkbox: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTextField.text = "Adress Email"
+        emailTextField.textColor = UIColor.gray
+        passwordTextField.text = "Hasło"
+        passwordTextField.textColor = UIColor.gray
+        passwordTextField.isSecureTextEntry = false
+        
+        
+        //#### When the user taps somewhere on the screen the keyboard toogles
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func checkboxClicked(_ sender: UIButton) {
-        
+        if checkbox.currentImage == UIImage.init(systemName: "square") {
+        checkbox.setImage(UIImage.init(systemName: "checkmark.square"), for: .normal)
+        } else {
+            checkbox.setImage(UIImage.init(systemName: "square"), for: .normal)
+        }
     }
     
     @IBAction func SignInButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "SignUpToMain", sender: self)
+        guard checkbox.currentImage == UIImage.init(systemName: "checkmark.square") else {
+            self.errorLabel.text = "! Prosze przeczytać regulamin !"
+            self.errorLabel.isHidden = false
+            return
+        }
+        
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    self.errorLabel.text = "! \(e.localizedDescription) !"
+                    self.errorLabel.isHidden = false
+                } else {
+                    self.performSegue(withIdentifier: "SignUpToMain", sender: self)
+                }
+            }
+            
+        }
+    }
+
+}
+
+//MARK: - UITextFieldDelegate
+extension SignUpController: UITextFieldDelegate{
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text! == "Hasło"{
+            passwordTextField.isSecureTextEntry = true
+        }
+        textField.textColor = UIColor.black
+        textField.text = ""
     }
     
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
