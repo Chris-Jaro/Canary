3c04e473daac642248c35f7d29b3a8ff23e72522
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2021-02-23 18:07:22 +0100

    Implement settings view (current user + log out & terms buttons + contact + redesign the view )

--- Kanarek/Controllers/SettingsController.swift
+++ Kanarek/Controllers/SettingsController.swift
@@ -11,10 +11,14 @@ import Firebase
 class SettingsController: UIViewController {
     
     let pushNotificationManager = PushNotificationManager()
-    
     let userDefaults = UserDefaults.standard // Accessing user defaults
-
     @IBOutlet weak var stateSwitch: UISwitch!
+    
+    @IBOutlet weak var currentUserLabel: UILabel!
+    @IBOutlet weak var mainSettingsView: UIView!
+    @IBOutlet weak var whiteLineView: UIView!
+    @IBOutlet weak var logOutButton: UIButton!
+    @IBOutlet weak var termsButton: UIButton!
     
     //## - Changes the color of battery and time an service to white
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -23,6 +27,24 @@ class SettingsController: UIViewController {
     
     //## Checks the settings and adjusts the switch state on the screen accordingly
     override func viewWillAppear(_ animated: Bool) {
+        //## These two lines round up the corners of the white line
+        whiteLineView.layer.cornerRadius = 10
+        mainSettingsView.layer.cornerRadius = 10
+        
+        //## These two lines round up the corners of the white line
+        logOutButton.layer.cornerRadius = 8
+        termsButton.layer.cornerRadius = 8
+        
+        //## To make the switch background white and rounded
+        stateSwitch.layer.cornerRadius = 15
+        stateSwitch.clipsToBounds = true
+    
+        //# This line sets the user to the current user If it exists (but it tecnically has to)
+        if let user = userDefaults.string(forKey: "UserEmail"){
+            currentUserLabel.text = user
+        }
+        
+        //## These lines set the switch state depending on the setting
         if let subscriptionSetting = userDefaults.string(forKey: "topicSubscription"){
             if subscriptionSetting.contains("Subscribed"){
                 stateSwitch.isOn = true
@@ -35,9 +57,6 @@ class SettingsController: UIViewController {
         super.viewDidLoad()
         
         stateSwitch.addTarget(self, action: #selector(stateChanged), for: .valueChanged) // sets up the observer of the switch - which triggers on value-change
-        //## To make the switch background white and rounded
-        stateSwitch.layer.cornerRadius = 15
-        stateSwitch.clipsToBounds = true
     }
     
     @IBAction func signOutButtonPressed(_ sender: UIButton) {
@@ -45,6 +64,11 @@ class SettingsController: UIViewController {
         userDefaults.removeObject(forKey: "UserEmail")
         
         navigationController?.dismiss(animated: true, completion: nil)
+    }
+    
+    //## preforms segue to the terms and conditions view
+    @IBAction func termsButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "GoToTermsFromSettings", sender: self)
     }
     
     //## Implements the functionality of the switch
