ae53c2b444abfa7caaf5d5dfcfbbaa7e328f7775
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2021-02-25 11:22:38 +0100

    Adjust and clean up the SignIn Controller Adjust and clean up the SignUp Controller (add spinnerView and sign-up in return button pressed)

--- Kanarek/Controllers/SignInController.swift
+++ Kanarek/Controllers/SignInController.swift
@@ -9,25 +9,25 @@ import Firebase
 
 class SignInController: UIViewController {
     
-    var vSpinner : UIView?
-    let userLoginDetails = UserDefaults.standard
-    
+    var loggingInView : UIView? //View displayed then the logging in process is taking place
+    let userLoginDetails = UserDefaults.standard // Accessing user defaults
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
     
-    
     //## - Changes the color of battery and time an service to white
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent
     }
+    //## - Function round the corners of 'log in' button and hides navigation bar
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
         signInButton.layer.cornerRadius = 10
         
     }
+    //## - Function reveals navigation bar, resets placeholders and error label
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.isNavigationBarHidden = false
@@ -36,6 +36,11 @@ class SignInController: UIViewController {
         errorLabel.text = "Error label"
     }
 
+    //## - Function is called after the view is loaded:
+        // -> sets placeholders for text fields
+        // -> performs auto-sign-in if there is a logged user(already registered that did not log out)
+        // -> sets up the tap gesutre for toggling the keyboard
+        // -> sets the textField delegates
     override func viewDidLoad() {
         super.viewDidLoad()
         setPlaceholder()
@@ -53,12 +58,18 @@ class SignInController: UIViewController {
         passwordTextField.delegate = self
     }
     
+    //## - Functions performs log-in when 'log in' button is pressed provided that text fields are not empty
     @IBAction func logInButtonPressed(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text{
             loggingIn(email: email, password: password)
         }
     }
     
+    //## - Function defines the login process:
+        // -> Displays spinnerView (to indicate loading process)
+        // -> Performs sign-in to Firebase Consol
+        // -- If fails -> print error
+        // -- If succees -> save login data for auto-sign-in remove spinner and perform segue to Main View
     func loggingIn(email:String, password:String){
         showSpinner(onView: self.view)
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
@@ -75,6 +86,7 @@ class SignInController: UIViewController {
         }
     }
     
+    //## - Function sets the placeholder in both text fields
     func setPlaceholder(){
         emailTextField.text = "Adress Email"
         passwordTextField.text = "Hasło"
@@ -85,34 +97,38 @@ class SignInController: UIViewController {
 }
 
 
-//MARK: - Loading Indicaiton
+//MARK: - Loading Indicaiton Methods
 extension SignInController {
+    //## - Function create the spinner view and displays it on top of self.view by setting it as loggingInView
     func showSpinner(onView : UIView) {
+        // The view and its background
         let spinnerView = UIView.init(frame: onView.bounds)
         spinnerView.backgroundColor = UIColor.init(red: 15/255, green: 139/255, blue: 205/255, alpha: 0.4)
-        let ai = UIActivityIndicatorView.init(style: .large)
-        ai.color = UIColor.white
-        ai.startAnimating()
-        ai.center = spinnerView.center
+        // The loading image ("activity indicator")
+        let activityIndicator = UIActivityIndicatorView.init(style: .large)
+        activityIndicator.color = UIColor.white
+        activityIndicator.startAnimating()
+        activityIndicator.center = spinnerView.center
         
         DispatchQueue.main.async {
-            spinnerView.addSubview(ai)
+            spinnerView.addSubview(activityIndicator)
             onView.addSubview(spinnerView)
         }
-        vSpinner = spinnerView
+        loggingInView = spinnerView
     }
     
+    //## - Functions that removes the spinnerView from self.view
     func removeSpinner() {
         DispatchQueue.main.async {
-            self.vSpinner?.removeFromSuperview()
-            self.vSpinner = nil
+            self.loggingInView?.removeFromSuperview()
+            self.loggingInView = nil
         }
     }
 }
 
-//MARK: - UITextFieldDelegate
+//MARK: - UITextFieldDelegate Methods
 extension SignInController: UITextFieldDelegate{
-    // Placeholder functionlaity
+    //## - Function handles the disapearance of the placeholder and adjusting text display when user strats editing
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField.text! == "Hasło"{
             textField.isSecureTextEntry = true
@@ -123,13 +139,13 @@ extension SignInController: UITextFieldDelegate{
         }
         textField.textColor = UIColor.black
     }
-    
+    //## - Function sets the placeholder back if the user left the textField empty
     func textFieldDidEndEditing(_ textField: UITextField) {
         if textField.text == ""{
             setPlaceholder()
         }
     }
-    
+    //## - Function toggles the keyboard when the user presses return button on the keyboard -> if the keyboard was editing the passowrd text filed when return is pressed the login function is triggered
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField.isSecureTextEntry == true{
             if let email = emailTextField.text, let password = passwordTextField.text{
--- Kanarek/Controllers/SignUpController.swift
+++ Kanarek/Controllers/SignUpController.swift
@@ -10,8 +10,8 @@ import Firebase
 
 class SignUpController: UIViewController{
     
-    let userLoginDetails = UserDefaults.standard
-    
+    var signingUpView: UIView? //View displayed then the signing-up process is taking place
+    let userLoginDetails = UserDefaults.standard //Accessing user defaults
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var checkbox: UIButton!
@@ -22,18 +22,21 @@ class SignUpController: UIViewController{
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent
     }
-    
+    //## - Rounding button corners
     override func viewWillAppear(_ animated: Bool) {
         signUpButton.layer.cornerRadius = 10
     }
-    
+    //## - Resetting error label and placeholders
     override func viewDidDisappear(_ animated: Bool) {
         setPlaceholder()
         errorLabel.isHidden = true
         errorLabel.text = ""
-//        navigationController?.popToRootViewController(animated: true)
     }
     
+    //## - Function is called after the view is loaded:
+        // -> sets placeholders for text fields
+        // -> sets up the tap gesutre for toggling the keyboard
+        // -> sets the textField delegates
     override func viewDidLoad() {
         super.viewDidLoad()
         setPlaceholder()
@@ -46,6 +49,7 @@ class SignUpController: UIViewController{
         passwordTextField.delegate = self
     }
     
+    //## - Functions handles clicking the 'I accept terms&conditions' checkbox
     @IBAction func checkboxClicked(_ sender: UIButton) {
         if checkbox.currentImage == UIImage.init(systemName: "square") {
         checkbox.setImage(UIImage.init(systemName: "checkmark.square"), for: .normal)
@@ -54,28 +58,45 @@ class SignUpController: UIViewController{
         }
     }
     
+    //## - Function handles clicking 'Sign up' button -> If text fields are not empty it triggers sign-up function
     @IBAction func SignUpButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            signingUp(email: email, password: password)
+        }
+    }
+    
+    //## - Function handles clicking 'Sign up' button:
+        // -> Displays and removes spinner on top of the views
+        // -> checks if the checkbox is selected
+        // -> performs Authentication.createUser function and creates user in Firebase consol
+        // -> saves the user's login data for Automatic SignIn
+        // -- If there are any error they are displayed on the error label
+        // -- If everything is successful it performs a segue to Main View
+    func signingUp(email: String, password: String){
+        showSpinner(onView: self.view)
         guard checkbox.currentImage == UIImage.init(systemName: "checkmark.square") else {
-            self.errorLabel.text = "! Prosze zaakceptować regulamin !"
+            self.errorLabel.text = "! Proszę zaakceptować regulamin !"
             self.errorLabel.isHidden = false
+            self.removeSpinner()
             return
         }
         
-        if let email = emailTextField.text, let password = passwordTextField.text{
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                if let e = error{
-                    self.errorLabel.text = "! \(e.localizedDescription) !"
-                    self.errorLabel.isHidden = false
-                } else {
-                    self.userLoginDetails.setValue(email, forKey: "UserEmail")
-                    self.userLoginDetails.setValue(password, forKey: "UserPassword")
-                    self.performSegue(withIdentifier: "SignUpToMain", sender: self)
-                }
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error{
+                self.errorLabel.text = "! \(e.localizedDescription) !"
+                self.errorLabel.isHidden = false
+                self.removeSpinner()
+            } else {
+                self.userLoginDetails.setValue(email, forKey: "UserEmail")
+                self.userLoginDetails.setValue(password, forKey: "UserPassword")
+                self.removeSpinner()
+                self.performSegue(withIdentifier: "SignUpToMain", sender: self)
             }
-            
         }
+        
     }
 
+    //## - Function sets the placeholder in both text fields
     func setPlaceholder(){
         emailTextField.text = "Adress Email"
         passwordTextField.text = "Hasło"
@@ -85,9 +106,38 @@ class SignUpController: UIViewController{
     }
 }
 
+//MARK: - Loading Indicaiton Methods
+extension SignUpController {
+    //## - Function create the spinner view and displays it on top of self.view by setting it as signingUpView
+    func showSpinner(onView : UIView) {
+        // The view and its background
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 15/255, green: 139/255, blue: 205/255, alpha: 0.4)
+        // The loading image ("activity indicator")
+        let activityIndicator = UIActivityIndicatorView.init(style: .large)
+        activityIndicator.color = UIColor.white
+        activityIndicator.startAnimating()
+        activityIndicator.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(activityIndicator)
+            onView.addSubview(spinnerView)
+        }
+        signingUpView = spinnerView
+    }
+    
+    //## - Functions that removes the spinnerView from self.view
+    func removeSpinner() {
+        DispatchQueue.main.async {
+            self.signingUpView?.removeFromSuperview()
+            self.signingUpView = nil
+        }
+    }
+}
+
 //MARK: - UITextFieldDelegate
 extension SignUpController: UITextFieldDelegate{
-    
+    //## - Function handles the disapearance of the placeholder and adjusting text display when user strats editing
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField.text! == "Hasło"{
             textField.isSecureTextEntry = true
@@ -98,14 +148,19 @@ extension SignUpController: UITextFieldDelegate{
         }
         textField.textColor = UIColor.black
     }
-    
+    //## - Function sets the placeholder back if the user left the textField empty
     func textFieldDidEndEditing(_ textField: UITextField) {
         if textField.text == ""{
             setPlaceholder()
         }
     }
-    
+    //## - Function toggles the keyboard when the user presses return button on the keyboard -> if the keyboard was editing the passowrd text filed when return is pressed the sign-up function is triggered
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.isSecureTextEntry == true{
+            if let email = emailTextField.text, let password = passwordTextField.text{
+                signingUp(email: email, password: password)
+            }
+        }
         textField.resignFirstResponder()
         return true
     }
