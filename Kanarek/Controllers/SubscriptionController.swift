9b02876d332f27902327643580d494517f54d3cc
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2021-03-19 15:43:54 +0100

    Implement spinnerView in SubscriptionController (to indicate the loading process -> purchasing or restoring purchase)

--- Kanarek/Controllers/SubscriptionController.swift
+++ Kanarek/Controllers/SubscriptionController.swift
@@ -10,6 +10,7 @@ import Purchases
 
 class SubscriptionController: UIViewController {
     
+    var performingPurchaseView : UIView? //View displayed when the loading process is taking place
     var packageForPurchase: Purchases.Package? //saving product
     let errorManager = ErrorManager() // accessing error-handling methods
     
@@ -65,18 +66,23 @@ class SubscriptionController: UIViewController {
     }
     
     ///# - Function is called when subscribeButton is pressed and performs actions:
+        // -> Shows spinnerView (to indicate to the user that clicking the button initiated some process and purchase is loading)
+            //$ spinnerView is removed when 1. purchase is successful, 2. purchase fails (removed before the errorAlert)
         // -> tries to perform a purchase of the chosen package (in this case there is only one product -> monthly subscription)
         // -> IF SUCCESS -> pops the SubscriptionView and allows the user to perform sign-in or sign-up]
         // -> IF User is already a subscriber -> performs restoration
         // -> IF user cancelled the purchase -> simply return
         // -> IF FAILURE -> displays the alert to the user with the error message
     @IBAction func subscribeButtonPressed(_ sender: UIButton) {
+        showSpinner(onView: self.view)
         if let package = packageForPurchase{
             Purchases.shared.purchasePackage(package) { (transaction, purchaserInfo, error, userCancelled) in
                 if purchaserInfo?.entitlements.all["fullAccess"]?.isActive == true {
                     // Unlock that great "pro" content -> pop the view -> check for subscription with viewWillAppear -> perform actions
+                    self.removeSpinner()
                     self.navigationController?.popViewController(animated: true)
                 } else {
+                    self.removeSpinner()
                     if userCancelled == true { return }
                     if let e = error{
                         self.errorManager.displayBasicAlert(title: "Błąd", subtitle: "Wystąpił błąd podczas dokonywania zakupu.\n Treść błędu: \(e.localizedDescription)", controller: self)
@@ -87,18 +93,23 @@ class SubscriptionController: UIViewController {
     }
     
     ///# Function is called when the restoreButton is pressed and performs action:
+        // -> Shows spinnerView (to indicate to the user that clicking the button initiated some process and purchaseRestoration is loading)
+            //$ spinnerView is removed (before the alert is displayed) when 1. restoration is successful, 2. restoration fails
         // -> Checks if current User has the entitlements
         // -> IF user is indeed a subscriber and has entitlements -> display a successAlert to the user (and on clicking "OK" button action) -> allow access to the app by popping SubscriptionView
         // -> IF user does not have the entitlements -> Only display the errorAlert to the user (there is no subscription data for this user)
     @IBAction func restoreButtonPressed(_ sender: UIButton) {
+        showSpinner(onView: self.view)
          Purchases.shared.restoreTransactions { (purchaserInfo, error) in
             if purchaserInfo?.entitlements.all["fullAccess"]?.isActive == true {
                 // IF successful
                 self.errorManager.displayBasicAlert(title: "Gotowe", subtitle: "Subskrypcja została odnowiona.", controller: self) { _ in
+                    self.removeSpinner()
                     self.navigationController?.popViewController(animated: true)
                 }
             } else {
                 // If the restoration process does not go as successfully, display the message to the user that there is no subscription to restore.
+                self.removeSpinner()
                 self.errorManager.displayBasicAlert(title: "Błąd", subtitle: "Nie znaleziono subskrypcji do odzyskania.", controller: self)
                 if let e = error {
                     print("Failed with error: \(e)")
@@ -107,4 +118,33 @@ class SubscriptionController: UIViewController {
          }
     }
     
+}
+
+//MARK: - Loading Spinner Methods
+extension SubscriptionController {
+    ///# - Function create the spinner view and displays it on top of self.view by setting it as performingPurchaseView
+    func showSpinner(onView : UIView) {
+        // The view and its background
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 15/255, green: 139/255, blue: 205/255, alpha: 0.4)
+        // The loading image ("activity indicator")
+        let activityIndicator = UIActivityIndicatorView.init(style: .large)
+        activityIndicator.color = UIColor.white
+        activityIndicator.startAnimating()
+        activityIndicator.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(activityIndicator)
+            onView.addSubview(spinnerView)
+        }
+        performingPurchaseView = spinnerView
+    }
+    
+    ///# - Functions that removes the spinnerView from self.view
+    func removeSpinner() {
+        DispatchQueue.main.async {
+            self.performingPurchaseView?.removeFromSuperview()
+            self.performingPurchaseView = nil
+        }
+    }
 }
